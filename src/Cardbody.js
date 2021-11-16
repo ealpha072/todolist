37d123d978d897526717ad34ea99b44e274b9c13
ealpha072 <ealpha072@gmail.com>
2021-11-16 20:34:28 +0300

    Complete todo styling

--- src/Cardbody.js
+++ src/Cardbody.js
@@ -3,20 +3,26 @@ import React, {useState} from 'react'
 //import CreateTodo from './CreateTodo'
 //import Todolist from './Todolist'
 
-function Todolist({todo}){
+function Todolist({todo, index, completeTodo}){
     return (
-           <div className={"card-header"} style={{marginBottom:"10px", borderRadius:"5px"}}>
-               {todo.text}
+           <div className={"card-header d-flex flex-row"} style={{marginBottom:"10px", borderRadius:"5px"}}>
+                <div className={""} style={{textDecoration: todo.isCompleted? "line-through":" "}}>{todo.text}</div>
+                <div className={"float-right"}> 
+                    <button className={"btn btn-sm btn-primary"} onClick={()=>completeTodo(index)}>Complete</button> 
+                    <button className={"btn btn-sm btn-primary"} ></button>
+                </div>
            </div>
     )
 }
 
-function TodoForm(addTodo){
+function TodoForm({addTodo}){
     const [inputText, setInputText] = useState('')
 
     const handleSubmit = (e) =>{
         e.preventDefault()
-        console.log('Hello')
+        if(!inputText) return
+        addTodo(inputText)
+        setInputText('')
     }
 
     return(
@@ -26,7 +32,7 @@ function TodoForm(addTodo){
                     <div className={"form-group row"}>
                         <label className="col-sm-2 col-form-label">New Todo</label>
                         <div className={"col-sm-7"}>
-                            <input type="text" className={"form-control"} placeholder="Add Todo..." required/>
+                            <input type="text" className={"form-control"} placeholder="Add Todo..." required value={inputText} onChange={(e)=>setInputText(e.target.value)}/>
                         </div>
                         <div className={"col-3"}>
                             <button className={"btn btn-primary"}>+ Add</button>
@@ -40,26 +46,32 @@ function TodoForm(addTodo){
 
 const Cardbody  = () => {
     const [todos, setTodos] = useState([
-        { id:1, text:'New commits'},
-        { id:2, text: "Meet friend for lunch" },
-        { id:3, text: "Build really cool todo app" }
+        { text:'New commits', isCompleted: false},
+        { text: "Meet friend for lunch", isCompleted: true },
+        { text: "Build really cool todo app", isCompleted: false }
     ])
 
-    const addTodo = (text)=>{
+    const addTodo = text =>{
         const newTodo = [...todos, {text}]
         setTodos(newTodo)
     }
 
+    const completeTodo = (index)=>{
+        const newTodos = [...todos]
+        newTodos[index].isCompleted = true
+        setTodos(newTodos)
+    }
+
     return(
         <>
-            <TodoForm />
+            <TodoForm addTodo={addTodo}/>
             <div className={"card"}>
                 <div className={"card-header"}>
                     <h6>Todo List</h6>
                 </div>
                 <div className={"card-body"}>
-                    {todos.map((todo)=>(
-                        <Todolist key={todo.id} todo={todo}/>
+                    {todos.map((todo, index)=>(
+                        <Todolist todo={todo} key={index} index={index} completeTodo={completeTodo}/>
                     ))}
                 </div>
             </div>
